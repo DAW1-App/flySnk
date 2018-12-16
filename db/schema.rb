87a3354536ef169ae03fcc47f45aacbe29bf4679
erodriguez96 <eduardo.rdgz@hotmail.com>
2018-12-16 19:05:42 +0000

    reinicio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_142746) do
+ActiveRecord::Schema.define(version: 2018_12_16_181824) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_142746) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "news", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "newsss", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "shoesName", limit: 200, null: false
     t.string "smallText", limit: 200
     t.string "largeText", limit: 1000
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_142746) do
     t.datetime "releaseDate"
     t.string "info", limit: 200
     t.binary "isActive", limit: 1, default: "b'1'"
+    t.timestamp "dateAdded", default: -> { "CURRENT_TIMESTAMP" }
   end
 
   create_table "sizes", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
