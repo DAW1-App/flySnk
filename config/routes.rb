5c376b785d4adb97b72e9a4a18daa11e865ef941
erodriguez96 <eduardo.rdgz@hotmail.com>
2018-12-17 21:27:35 +0000

    sidebar corrected, about added, minor bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/about'
   get 'news/admin_new'
   resources :news
   get 'shoes/admin_shoe'
--- db/migrate/20181216190820_create_news.rb
+++ db/migrate/20181216190820_create_news.rb
@@ -2,8 +2,8 @@ class CreateNews < ActiveRecord::Migration[5.2]
   def change
     create_table :news do |t|
       t.string :shoeName
-      t.string :smallText
-      t.string :largeText
+      t.text :smallText
+      t.text :largeText
       t.datetime :releaseDate
       t.integer :price
       t.string :color
